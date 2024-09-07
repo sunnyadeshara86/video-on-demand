b56bf8f55b5ced559130a32fde0f114e0889e8cb
sunny.a.adeshara@outlook.com <sunnyadeshara@outlook.com>
2024-09-07 17:26:53 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+import { Sidebar } from "./components/Sidebar";
+
+function App() {
+  return <div>
+  <NavBar />
+  <Sidebar />
+</div>;
+}
+
+export default App;
